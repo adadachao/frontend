cae6c3758306e0c0648815a148749937856b7af3
web <adadachao@gmail.com>
2025-08-04 10:32:58 +0800

    up

--- configs/app/features/account.ts
+++ configs/app/features/account.ts
@@ -1,18 +1,20 @@
 import type { Feature } from './types';
 
-import services from '../services';
-import { getEnvValue } from '../utils';
+// import services from '../services';
+// import { getEnvValue } from '../utils';
 
 const title = 'My account';
 
 const config: Feature<{ isEnabled: true; recaptchaSiteKey: string }> = (() => {
-  if (getEnvValue('NEXT_PUBLIC_IS_ACCOUNT_SUPPORTED') === 'true' && services.reCaptchaV2.siteKey) {
-    return Object.freeze({
-      title,
-      isEnabled: true,
-      recaptchaSiteKey: services.reCaptchaV2.siteKey,
-    });
-  }
+  // 禁用account功能，隐藏Log in按钮
+
+  // if (getEnvValue('NEXT_PUBLIC_IS_ACCOUNT_SUPPORTED') === 'true' && services.reCaptchaV2.siteKey) {
+  //   return Object.freeze({
+  //     title,
+  //     isEnabled: true,
+  //     recaptchaSiteKey: services.reCaptchaV2.siteKey,
+  //   });
+  // }
 
   return Object.freeze({
     title,
--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -7,9 +7,7 @@ import type { CustomLinksGroup } from 'types/footerLinks';
 
 import config from 'configs/app';
 import type { ResourceError } from 'lib/api/resources';
-import useApiQuery from 'lib/api/useApiQuery';
 import useFetch from 'lib/hooks/useFetch';
-import useIssueUrl from 'lib/hooks/useIssueUrl';
 import { Link } from 'toolkit/chakra/link';
 import { Skeleton } from 'toolkit/chakra/skeleton';
 import { CONTENT_MAX_WIDTH } from 'ui/shared/layout/utils';
@@ -21,50 +19,18 @@ const MAX_LINKS_COLUMNS = 4;
 
 const Footer = () => {
 
-  const { data: backendVersionData } = useApiQuery('general:config_backend_version', {
-    queryOptions: {
-      staleTime: Infinity,
-      enabled: !config.features.opSuperchain.isEnabled,
-    },
-  });
-  const issueUrl = useIssueUrl(backendVersionData?.backend_version);
-
   const BLOCKSCOUT_LINKS = [
-    {
-      icon: 'edit' as const,
-      iconSize: '16px',
-      text: 'Submit an issue',
-      url: issueUrl,
-    },
-    {
-      icon: 'social/git' as const,
-      iconSize: '18px',
-      text: 'Contribute',
-      url: 'https://github.com/blockscout/blockscout',
-    },
     {
       icon: 'social/twitter' as const,
       iconSize: '18px',
       text: 'X (ex-Twitter)',
-      url: 'https://x.com/blockscout',
+      url: 'https://x.com/movachain',
     },
     {
-      icon: 'social/discord' as const,
-      iconSize: '24px',
-      text: 'Discord',
-      url: 'https://discord.gg/blockscout',
-    },
-    {
-      icon: 'brands/blockscout' as const,
+      icon: 'social/telega' as const,
       iconSize: '18px',
-      text: 'All chains',
-      url: 'https://www.blockscout.com/chains-and-projects',
-    },
-    {
-      icon: 'donate' as const,
-      iconSize: '20px',
-      text: 'Donate',
-      url: 'https://eth.blockscout.com/address/0xfB4aF6A8592041E9BcE186E5aC4BDbd2B137aD11',
+      text: 'Telegram',
+      url: 'https://t.me/MovaChain',
     },
   ];
 
@@ -188,8 +154,8 @@ const Footer = () => {
           gap={ 1 }
           gridTemplateColumns={{
             base: 'repeat(auto-fill, 160px)',
-            lg: 'repeat(2, 160px)',
-            xl: 'repeat(3, 160px)',
+            lg: 'repeat(1, 160px)', //2
+            xl: 'repeat(1, 160px)', //3
           }}
           gridTemplateRows={{
             base: 'auto',
